bba029b292e2bfae296e575ac66b34d861d0f67d
Joao Vitor <joaovitorvogel85@gmail.com>
2023-09-01 14:53:42 -0300

    add

--- Ferroviaria/src/main/java/org/example/Main.java
+++ Ferroviaria/src/main/java/org/example/Main.java
@@ -4,11 +4,18 @@ import java.util.*;
 public class Main {
     static Scanner keyboard = new Scanner(System.in);
     static Vagao[] garagemVagao = new Vagao[100];
+    static Locomotiva[] garageLocomotiva = new Locomotiva[10];
     public static void main(String[] args) {
-        
-
         for(int i = 0; i < 100; i++){
             garagemVagao[i] = new Vagao(i, 100, 0);
+        }
+        for(int i = 0; i < 10; i++){
+            garageLocomotiva[i] = new Locomotiva(i, 1500, 15, 0);
+        }
+
+        //coloca no menu
+        for(int i = 0; i < 10; i++){
+            System.out.println(garageLocomotiva.toString());
         }
 
         while(menu());    
